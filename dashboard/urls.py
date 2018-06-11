69b4ec145b3b282c37e8bc67d01200f844e76279
David Okwii <dokwii@i3c.co.ug>
2018-06-11 17:09:15 +0300

    added urls to sidebar items

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,4 +3,9 @@ from dashboard import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='dashboard-home'),
+	url(r'^admin_profile/$', views.admin_profile, name='dashboard-admin-profile'),
+	url(r'^icons/$', views.icons, name='dashboard-icons'),
+	url(r'^tables/$', views.tables, name='dashboard-tables'),
+	url(r'^notifs/$', views.notifications, name='dashboard-notifs'),
+	url(r'^upgrade/$', views.upgrade, name='dashboard-upgrade'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -4,3 +4,18 @@ from django.shortcuts import render
 
 def index(request):
 	return render(request, 'dashboard.html')
+
+def admin_profile(request):
+	return render(request, 'admin_profile.html')
+
+def tables(request):
+	return render(request, 'tables.html')
+
+def icons(request):
+	return render(request, 'icons.html')
+
+def notifications(request):
+	return render(request, 'notifications.html')
+
+def upgrade(request):
+	return render(request, 'upgrade.html')
